e5b56b80d4b49f0cd6580a8d50c7952a08085961
Daniel Lubarov <daniel@lubarov.com>
2014-09-18 16:05:07 -0700

    Fix displayed test names.
    The `BurstTestCase` approach didn't work as desired - the tests were displayed as `BurstTestCase` regardless of what `getName` or `toString` returned. Something in the framework must be doing `getClass().getName()` or similar, though I haven't found the code that does so.
    
    So it seems we can't use "wrapper" `TestCase`s, making it a bit harder to control test names. Using `setTest` would break `TestCase`s reflective method invokation. Overriding `getTest` works, but we don't want to require a single parent test case, so it seems best to instrument test classes at runtime.

--- burst-android-test/instrumentation/src/main/java/com/example/burst/MethodTest.java
+++ burst-android-test/instrumentation/src/main/java/com/example/burst/MethodTest.java
@@ -2,7 +2,7 @@ package com.example.burst;
 
 import junit.framework.TestCase;
 
-public final class MethodTest extends TestCase {
+public class MethodTest extends TestCase {
   public void testSingleParameter(Drink drink) {
     // TODO this currently is not supported!
     assertNotNull(drink);
