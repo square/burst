ef301f9a72f5ff9769849c0447d3fa3e6436c8bc
Daniel Lubarov <daniel@lubarov.com>
2015-02-03 17:49:09 -0800

    Drop JUnit 3 support.
    Now that we have `RegisterInstrumentationRunner` with JUnit 4 support, you can write JUnit 4 tests with `@RunWith(BurstJUnit4.class)`. We've tested this internally and it works fine.

--- burst-android-test/instrumentation/src/main/java/com/example/burst/Drink.java
+++
@@ -1,7 +0,0 @@
-package com.example.burst;
-
-public enum Drink {
-  COKE,
-  PEPSI,
-  RC_COLA
-}
--- burst-android-test/instrumentation/src/main/java/com/example/burst/NormalTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.burst;
-
-import junit.framework.TestCase;
-
-public class NormalTest extends TestCase {
-  public void testNothingInParticular() {
-    assertTrue(true);
-  }
-}
